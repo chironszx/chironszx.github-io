c479bf0be9be3303c832abf5ef526dbc2aa44449
chiron <yanmiguelbrandao966@gmail.com>
2024-12-24 18:52:27 -0300

    automação de ir até o youtube
    uma simples automação que faz voce ir até o youtube de forma simples

---
+++ autom.py
@@ -0,0 +1,13 @@
+import pyautogui as pa
+import time
+pa.PAUSE=1
+pa.press("win")
+pa.press("ENTER")
+pa.write("edge")
+pa.press("ENTER")
+pa.press("ENTER")
+pa.write("youtube")
+pa.click(x=371, y=141)
+pa.click(x=295, y=492)
+
+
